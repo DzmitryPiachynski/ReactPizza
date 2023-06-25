22ee8eda92e4b74a057e4c9df49e7de3de181643
Dzmitry Piachynski <pechinskiy.dima@gmail.com>
2023-06-25 19:16:49 +0200

    Pizza container and fake backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
@@ -6,12 +6,20 @@ import {Route, Routes} from 'react-router-dom'
 
 function App() {
 
+    const [pizza, setPizza] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json')
+            .then(res => res.json())
+            .then(json => setPizza(json.pizzas))
+    }, [])
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home items={pizza} />} />
                     <Route path="/cart" element={<Cart />} />
                 </Routes>
             </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import {Header} from './Header'
 import {Categories} from './Categories';
 import {SortPopup} from './SortPopup'
 import {Button} from "./Button";
+import {PizzaBlock} from "./PizzaBlock";
 
-export {Header, Button, Categories, SortPopup}
+export {Header, Button, Categories, SortPopup, PizzaBlock}
